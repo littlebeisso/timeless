0bcfb4326f8023ed6f6796e4f5be3ca3abc42a8c
littlebeisso <jpdeoliveira648@gmail.com>
2025-02-22 10:25:19 -0300

    calculadoradeareadoretangulo.cs

---
+++ calculadora de area do retangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main()
+    {
+        Console.WriteLine ("Calculadora de Área do Retângulo");
+
+        Console.Write("Insira a altura do retângulo: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Insira a largura do retângulo: ");
+        double largura = Convert.ToDouble(Console.ReadLine());
+
+        double area = altura * largura;
+
+        Console.WriteLine("A área do retângulo é " + area + "m2");
+    }
+}
