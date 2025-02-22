1d2596d66f2c18b6811d31896d3e358a7100364b
littlebeisso <jpdeoliveira648@gmail.com>
2025-02-22 08:19:33 -0300

    Conversor de Temperatura em C#

---
+++ Conversor de Temperatura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Seja bem vindo usuário 01!");
+        Console.WriteLine("Está calor na sua cidade ?");
+        
+        Console.Write("Digite a temperatura em  °C: ");
+        double temperatureCelsius = Convert.ToDouble(Console.ReadLine());
+
+        double temperatureFahrenreit = (temperatureCelsius * 9 / 5) + 32;
+
+        Console.WriteLine("A temperatura na sua cidade em Fahrenheit está em:" + temperatureFahrenreit + "°F" );
+
+
+    }
+
+
+}
